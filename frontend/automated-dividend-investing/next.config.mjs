3ac0dbb9eaa0068d204a93c5b3bab2d13ad878a2
cobriensr <charles.a.obrien@outlook.com>
2024-10-13 17:47:14 -0500

    updated next config file for gunicorn

--- frontend/automated-dividend-investing/next.config.mjs
+++ frontend/automated-dividend-investing/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+};
 
 export default nextConfig;
