96b697fb005552a554345fceb2b9a95490aee95f
cobriensr <charles.a.obrien@outlook.com>
2024-10-16 11:09:56 -0500

    added api response mappings

---
+++ app/utils/mappings.py
@@ -0,0 +1,104 @@
+# pylint: disable = missing-module-docstring, line-too-long, missing-final-newline
+
+# industry api response mapping
+industry_mapping = {
+    "Excel Industry Name": "API Industry Name",
+    "Life Sciences Tools and Services": "Medical - Diagnostics & Research",
+    "Technology Hardware, Storage and Peripherals": "Computer Hardware",
+    "Biotechnology": "Biotechnology",
+    "Commercial Services and Supplies": "Specialty Business Services",
+    "Mortgage Real Estate Investment Trusts (REITs)": "REIT - Mortgage",
+    "Healthcare Equipment and Supplies": "Medical - Devices",
+    "IT Services": "Information Technology Services",
+    "Banks": "Banks - Diversified",
+    "Semiconductors and Semiconductor Equipment": "Semiconductors",
+    "Food Products": "Packaged Foods",
+    "Multi-Utilities": "Diversified Utilities",
+    "Electric Utilities": "Regulated Electric",
+    "Independent Power and Renewable Electricity Producers": "Independent Power Producers",
+    "Insurance": "Insurance - Diversified",
+    "Machinery": "Industrial - Machinery",
+    "Thrifts and Mortgage Finance": "Financial - Mortgages",
+    "Trading Companies and Distributors": "Industrial - Distribution",
+    "Chemicals": "Chemicals",
+    "Building Products": "Construction Materials",
+    "Diversified Financial Services": "Financial - Conglomerates",
+    "Containers and Packaging": "Packaging & Containers",
+    "Electrical Equipment": "Electrical Equipment & Parts",
+    "Capital Markets": "Financial - Capital Markets",
+    "Electronic Equipment, Instruments and Components": "Electronic Equipment, Instruments and Components",
+    "Equity Real Estate Investment Trusts (REITs)": "REIT - Diversified",
+    "Water Utilities": "Regulated Water",
+    "Gas Utilities": "Regulated Gas",
+    "Specialty Retail": "Specialty Retail",
+    "Leisure Products": "Leisure",
+    "Paper and Forest Products": "Paper, Lumber & Forest Products",
+    "Beverages": "Beverages - Non-Alcoholic",
+    "Media": "Broadcasting",
+    "Aerospace and Defense": "Aerospace & Defense",
+    "Food and Staples Retailing": "Grocery Stores",
+    "Diversified Telecommunication Services": "Telecommunications Services",
+    "Hotels, Restaurants and Leisure": "Restaurants",
+    "Air Freight and Logistics": "Integrated Freight & Logistics",
+    "Road and Rail": "Railroads",
+    "Oil, Gas and Consumable Fuels": "Oil & Gas Integrated",
+    "Professional Services": "Consulting Services",
+    "Communications Equipment": "Communication Equipment",
+    "Industrial Conglomerates": "Conglomerates",
+    "Multiline Retail": "Department Stores",
+    "Consumer Finance": "Financial - Credit Services",
+    "Household Durables": "Furnishings, Fixtures & Appliances",
+    "Internet and Direct Marketing Retail": "Internet Content & Information",
+    "Healthcare Providers and Services": "Medical - Healthcare Plans",
+    "Textiles, Apparel and Luxury Goods": "Apparel - Manufacturers",
+    "Auto Components": "Auto - Parts",
+    "Marine": "Marine Shipping",
+    "Tobacco": "Tobacco",
+    "Personal Products": "Personal Products & Services",
+    "Construction Materials": "Construction Materials",
+    "Software": "Software - Application",
+    "Metals and Mining": "Other Precious Metals",
+    "Automobiles": "Auto - Manufacturers",
+    "Energy Equipment and Services": "Oil & Gas Equipment & Services",
+    "Real Estate Management and Development": "Real Estate - Services",
+    "Diversified Consumer Services": "Specialty Business Services",
+    "Construction and Engineering": "Engineering & Construction",
+    "Distributors": "Industrial - Distribution",
+    "Household Products": "Household Products",
+    "Pharmaceuticals": "Medical - Pharmaceuticals"
+}
+
+# sector api response mapping
+sector_mapping = {
+    "Excel Sector Name": "API Sector Name",
+    "Health Care": "Healthcare",
+    "Information Technology": "Technology",
+    "Industrials": "Industrials",
+    "Financials": "Financial Services",
+    "Consumer Staples": "Consumer Defensive",
+    "Utilities": "Utilities",
+    "Materials": "Basic Materials",
+    "Real Estate": "Real Estate",
+    "Consumer Discretionary": "Consumer Cyclical",
+    "Communication Services": "Communication Services",
+    "Energy": "Energy"
+}
+
+# instituional ownership api response mapping
+institutional_ownership_mapping = {
+    "Very Low": "0-20%",
+    "Low": "21-40%",
+    "Moderate": "41-60%",
+    "High": "61-80%",
+    "Very High": "81-100%" 
+}
+
+# insider transaction type list
+insider_tran_type_list = [
+	"J-Other",
+	"P-Purchase",
+	"W-Will",
+	"I-Discretionary",
+	"Z-Trust",
+	"F-InKind"
+]
